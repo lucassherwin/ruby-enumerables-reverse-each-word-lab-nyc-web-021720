d86e674a4aededc1eed8f0ec22b91b6498c0d9ed
Lucas Sherwin <lksherwin@gmail.com>
2020-01-30 19:02:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
-def reverse_each_word(word)
-  word_arr = word.split(" ")
-  word_arr.map{ |a| a.reverse}
-  word_arr
+def reverse_each_word(sentence) 
+  split_sentence = sentent.split.collect(" ")
+  split_sentence.reverse
+  
 end
